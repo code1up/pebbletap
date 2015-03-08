16972cf449c2e2ca66d1da57542010f0e3625d78
code1up <dev.code1up@googlemail.com>
2015-03-08 12:22:44 +0000

    wip

--- src/app.js
+++ src/app.js
@@ -1,58 +1,56 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
-var UI = require('ui');
-var Vector2 = require('vector2');
+var UI = require("ui");
+var Vector2 = require("vector2");
 
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  title: "Pebble.js",
+  icon: "images/menu_icon.png",
+  subtitle: "Hello World!",
+  body: "Press any button."
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
+setTimeout(function () {
+  main.title = "wibble";
+}, 2000);
+
+main.on("click", "up", function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
+        title: "Pebble.js",
+        icon: "images/menu_icon.png",
+        subtitle: "Can do Menus"
       }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
+        title: "Second Item",
+        subtitle: "Subtitle Text"
       }]
     }]
   });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
+  menu.on("select", function(e) {
+    console.log("Selected item #" + e.itemIndex + " of section #" + e.sectionIndex);
+    console.log("The item is titled'"  + e.item.title + "'");
   });
   menu.show();
 });
 
-main.on('click', 'select', function(e) {
+main.on("click", "select", function(e) {
   var wind = new UI.Window();
   var textfield = new UI.Text({
     position: new Vector2(0, 50),
     size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
+    font: "gothic-24-bold",
+    text: "Text Anywhere!",
+    textAlign: "center"
   });
   wind.add(textfield);
   wind.show();
 });
 
-main.on('click', 'down', function(e) {
+main.on("click", "down", function(e) {
   var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
+  card.title("A Card");
+  card.subtitle("Is a Window");
+  card.body("The simplest window type in Pebble.js.");
   card.show();
 });
